15fa28fc40f4896f1753bb1f23972e0633eaa435
Prasanna <prasanna-hexaware@github.com>
2019-11-26 13:18:26 +0530

    ts

---
+++ Dhanush/P14.ts
@@ -0,0 +1,6 @@
+var  num:number = -5
+if (num > 0) { 
+   console.log("number is positive") 
+} else {
+    console.log("Number is negative")
+}
---
+++ Dhanush/P6.js
@@ -0,0 +1,4 @@
+let no = 10; 
+let no = 20;
+
+console.log(no1);
