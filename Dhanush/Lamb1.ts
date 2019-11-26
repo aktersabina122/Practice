26538954a7136167430e4fb14d2c4a3bedf9b024
Prasanna <prasanna-hexaware@github.com>
2019-11-26 13:37:10 +0530

    ts

---
+++ Dhanush/Lamb1.ts
@@ -0,0 +1,11 @@
+let sum = (x: number, y: number): number => {
+    return x + y;
+}
+
+let Print = () => console.log("Hello TypeScript");
+
+let sum1 = (x: number, y: number) => x + y;
+
+sum1(3,4); //returns 30
+
+console.log(sum(10, 20)); //returns 30
