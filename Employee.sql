34a5f337fa692c1bd64b0fe69856b3f11bcef10a
Prasanna <prasanna-hexaware@github.com>
2019-12-20 09:51:33 +0530

    ch

---
+++ Employee.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `employee` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(20) DEFAULT NULL,
+  `last_name` varchar(20) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
